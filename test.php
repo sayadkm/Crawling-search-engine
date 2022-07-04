812ec6031297135761b8fe7d59241fb290fd6935
sayd km <sayadkm1988@gmail.com>
2022-07-04 20:27:38 +0530

    Updating Warning issue

--- test.php
+++ test.php
@@ -1,11 +1,14 @@
 <?php
 require_once "src/SearchEngine.php";
 
-$rq = new \SearchEngine\SearchEngine();
+use SearchEngine\SearchEngine;
+
+$rq = new SearchEngine();
 
 $rq->setEngine('google.com');
 
 $res = $rq->search(array('Flipcart'));
+
 echo '<pre>';
 print_r($res);
 ?>
